61fabf8db362507ca23bc10a52f772daf3990c69
AnitaMalek <anitamalek1991@gmail.com>
2019-01-26 12:32:03 +0100

    dodaje

---
+++ src/main/java/sda/App.java
@@ -0,0 +1,18 @@
+package sda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+
+        User user = new User();
+        user.setName("Anita");
+        user.setLastName("Malek");
+        user.setLogin("AMalek");
+        user.setPassword("admin");
+
+
+    }
+}
--- src/main/java/sda/User.java
+++ src/main/java/sda/User.java
@@ -2,6 +2,43 @@ package sda;
 
 public class User {
 
-    private String
+    private String name;
+    private String lastName;
+    private String login;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 
 }
+
