3ccb28e836d45b355edf2594aa032406cde30009
AnitaMalek <anitamalek1991@gmail.com>
2019-01-27 14:31:12 +0100

    Country

---
+++ src/main/java/sda/Country.java
@@ -0,0 +1,19 @@
+package sda;
+
+import java.util.Set;
+
+public class Country {
+
+private String nazwa;
+private String stolica;
+private int liczbaLudnosci;
+
+public Country(String nazwa, String stolica, int liczbaLudnosci){
+    this.nazwa = nazwa;
+    this.stolica = stolica;
+    this.liczbaLudnosci = liczbaLudnosci;
+}
+
+
+
+}
